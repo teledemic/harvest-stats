7b8869458c0f803664a14e08c7651fe09fba5077
Carl Irving <carl@teledemic.com>
2018-04-16 23:23:57 -0400

    Switch to csv-parse es5

--- src/csvreader.js
+++ src/csvreader.js
@@ -1,11 +1,11 @@
-import csv from 'csv';
+import csvparse from 'csv-parse/lib/es5';
 
 export default class CSVReader {
   static ReadFile(buffer) {
     return new Promise((resolve, reject) => {
       const reader = new FileReader();
       reader.onload = () => {
-        csv.parse(reader.result, (err, data) => {
+        csvparse(reader.result, (err, data) => {
           console.log(data[0]);
           resolve(data);
         });
